001364077485b9948ef58a89dafd2b0d37643df1
Tyler Bray <tylerhbray@gmail.com>
2024-04-17 15:29:23 -0700

    edits

--- view/src/Pages/Components/Navbar.js
+++ view/src/Pages/Components/Navbar.js
@@ -10,7 +10,7 @@ function Navbar() {
             <div class="flex-shrink-0">
               <NavLink to="/">
                 <img
-                  class="h-8 w-8 rounded-full m-3"
+                  class="h-8 w-8 rounded m-3"
                   src={myImage}
                   alt="Your Company"
                 />
