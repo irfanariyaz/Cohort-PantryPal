8cfdab5f4a9296de822924acb161087ba760ca5d
Tyler Bray <tylerhbray@gmail.com>
2024-04-17 15:14:07 -0700

    edits

--- view/src/Pages/Home.js
+++ view/src/Pages/Home.js
@@ -1,23 +1,25 @@
 import { Link } from "react-router-dom";
+import myImage from "../img.png";
 
 function Home() {
   return (
     <>
-      <section class="bg-white dark:bg-gray-900">
+      <section class="bg-green-100 dark:bg-green-900">
         <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
           <div class="mr-auto place-self-center lg:col-span-7">
-            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
-              Payments tool for software companies
+            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-gray-900 dark:text-white">
+              Your Pantry, Reimagined.
             </h1>
-            <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
-              From checkout to global sales tax compliance, companies around the
-              world use Flowbite to simplify their payment stack.
+            <p class="max-w-2xl mb-6 font-light text-gray-600 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-300">
+              Effortlessly manage your grocery list, discover recipes, and savor
+              the joy of home-cooked meals. With Pantry Pal, every ingredient
+              counts, from the store to your door.
             </p>
             <Link
               to="/dashboard"
-              className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+              className="inline-flex items-center justify-center pe-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg hover:text-green-600 focus:ring-4 focus:ring-green-300 dark:focus:ring-green-800"
             >
-              Get started
+              Start Simplifying
               <svg
                 className="w-5 h-5 ml-2 -mr-1"
                 fill="currentColor"
@@ -32,32 +34,13 @@ function Home() {
               </svg>
             </Link>
           </div>
-          <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
-            <img
-              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png"
-              alt="mockup"
-            ></img>
+          <div class="hidden lg:flex lg:col-span-5 lg:justify-center">
+            <img src={myImage} class=" w-[85%]" alt="mockup"></img>
           </div>
         </div>
       </section>
       <section class="bg-white dark:bg-gray-200">
         <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
-          <div class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
-            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
-              We didn't reinvent the wheel
-            </h2>
-            <p class="mb-4">
-              We are strategists, designers and developers. Innovators and
-              problem solvers. Small enough to be simple and quick, but big
-              enough to deliver the scope you want at the pace you need. Small
-              enough to be simple and quick, but big enough to deliver the scope
-              you want at the pace you need.
-            </p>
-            <p>
-              We are strategists, designers and developers. Innovators and
-              problem solvers. Small enough to be simple and quick.
-            </p>
-          </div>
           <div class="grid grid-cols-2 gap-4 mt-8">
             <img
               class="w-full rounded-lg"
@@ -70,14 +53,46 @@ function Home() {
               alt="office content 2"
             ></img>
           </div>
+          <div class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+            <h2 class="mb-5 text-4xl tracking-tight font-extrabold text-green-700">
+              Simplifying Your Grocery Game
+            </h2>
+            <p class="mb-5 text-gray-700">
+              At Pantry Pal, we streamline your shopping and meal prep. Blending
+              strategy, design, and tech, we craft a tool that's as efficient as
+              it is easy to use—getting you from pantry to plate without the
+              hassle.
+            </p>
+            <p class="mb-5 text-gray-700">
+              Fresh thinking, simplified shopping.
+            </p>
+            <Link
+              to="/dashboard"
+              className="inline-flex items-center justify-center pe-5 mr-3 text-base font-medium text-center text-green-900 rounded-lg bg-primary-700 hover:text-sky-600 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+            >
+              Start Simplifying
+              <svg
+                className="w-5 h-5 ml-2 -mr-1"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </Link>
+          </div>
         </div>
       </section>
-      <section class="bg-white dark:bg-gray-900">
+      <section class="bg-white dark:bg-green-900">
         <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
           <h2 class="mb-8 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
             Frequently asked questions
           </h2>
-          <div class="grid pt-8 text-left border-t border-gray-200 md:gap-16 dark:border-gray-700 md:grid-cols-2">
+          <div class="grid pt-8 text-left border-t border-gray-100 md:gap-16 dark:border-gray-700 md:grid-cols-2">
             <div>
               <div class="mb-10">
                 <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
@@ -93,13 +108,13 @@ function Home() {
                       clip-rule="evenodd"
                     ></path>
                   </svg>
-                  What do you mean by "Figma assets"?
+                  What exactly is "Grocery Management" in the app?
                 </h3>
                 <p class="text-gray-500 dark:text-gray-400">
-                  You will have access to download the full Figma project
-                  including all of the pages, the components, responsive pages,
-                  and also the icons, illustrations, and images included in the
-                  screens.
+                  Our Grocery Management feature allows you to input and track
+                  the items in your pantry. You'll be able to see at a glance
+                  what you have, what you’re running low on, and what you need
+                  for your next grocery haul.
                 </p>
               </div>
               <div class="mb-10">
@@ -116,12 +131,13 @@ function Home() {
                       clip-rule="evenodd"
                     ></path>
                   </svg>
-                  What does "lifetime access" exactly mean?
+                  Is it possible to track my dietary needs with the app?
                 </h3>
                 <p class="text-gray-500 dark:text-gray-400">
-                  Once you have purchased either the design, code, or both
-                  packages, you will have access to all of the future updates
-                  based on the roadmap, free of charge.
+                  Definitely. Whether you're counting carbs, keeping an eye on
+                  protein intake, or balancing fats, our app's macro counter and
+                  recipe analyzer will help you stay on track with your dietary
+                  goals, making healthy eating simpler.
                 </p>
               </div>
               <div class="mb-10">
@@ -138,58 +154,13 @@ function Home() {
                       clip-rule="evenodd"
                     ></path>
                   </svg>
-                  How does support work?
+                  How does the "Recipe Integration" work?
                 </h3>
                 <p class="text-gray-500 dark:text-gray-400">
-                  We're aware of the importance of well qualified support, that
-                  is why we decided that support will only be provided by the
-                  authors that actually worked on this project.
-                </p>
-                <p class="text-gray-500 dark:text-gray-400">
-                  Feel free to{" "}
-                  <a
-                    href="#"
-                    class="font-medium underline text-primary-600 dark:text-primary-500 hover:no-underline"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    contact us
-                  </a>{" "}
-                  and we'll help you out as soon as we can.
-                </p>
-              </div>
-              <div class="mb-10">
-                <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
-                  <svg
-                    class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                      clip-rule="evenodd"
-                    ></path>
-                  </svg>
-                  I want to build more than one project. Is that allowed?
-                </h3>
-                <p class="text-gray-500 dark:text-gray-400">
-                  You can use Windster for an unlimited amount of projects,
-                  whether it's a personal website, a SaaS app, or a website for
-                  a client. As long as you don't build a product that will
-                  directly compete with Windster either as a UI kit, theme, or
-                  template, it's fine.
-                </p>
-                <p class="text-gray-500 dark:text-gray-400">
-                  Find out more information by{" "}
-                  <a
-                    href="#"
-                    class="font-medium underline text-primary-600 dark:text-primary-500 hover:no-underline"
-                  >
-                    reading the license
-                  </a>
-                  .
+                  Select recipes you're interested in, and our app will
+                  automatically cross-reference the ingredients with your
+                  current pantry inventory. It'll then generate a shopping list
+                  for any missing items, streamlining your meal prep planning.
                 </p>
               </div>
             </div>
@@ -208,19 +179,14 @@ function Home() {
                       clip-rule="evenodd"
                     ></path>
                   </svg>
-                  What does "free updates" include?
+                  Can the app suggest recipes based on what's already in my
+                  fridge?
                 </h3>
                 <p class="text-gray-500 dark:text-gray-400">
-                  The free updates that will be provided is based on the{" "}
-                  <a
-                    href="#"
-                    class="font-medium underline text-primary-600 dark:text-primary-500 hover:no-underline"
-                  >
-                    roadmap
-                  </a>{" "}
-                  that we have laid out for this project. It is also possible
-                  that we will provide extra updates outside of the roadmap as
-                  well.
+                  Absolutely! Just input the groceries you have, and our app
+                  will suggest a variety of recipes you can make without needing
+                  a trip to the store. It's perfect for those times you're
+                  unsure what to cook with what you have.
                 </p>
               </div>
               <div class="mb-10">
@@ -237,19 +203,13 @@ function Home() {
                       clip-rule="evenodd"
                     ></path>
                   </svg>
-                  What does the free version include?
+                  What does "macro counter" mean for my recipes?
                 </h3>
                 <p class="text-gray-500 dark:text-gray-400">
-                  The{" "}
-                  <a
-                    href="#"
-                    class="font-medium underline text-primary-600 dark:text-primary-500 hover:no-underline"
-                  >
-                    free version
-                  </a>{" "}
-                  of Windster includes a minimal style guidelines, component
-                  variants, and a dashboard page with the mobile version
-                  alongside it.
+                  Each recipe in our app comes with a detailed breakdown of
+                  macronutrients - that's your proteins, carbs, fats, and more.
+                  It's designed to help you meet dietary goals and stay informed
+                  about the nutritional content of your meals.
                 </p>
                 <p class="text-gray-500 dark:text-gray-400">
                   You can use this version for any purposes, because it is
@@ -270,55 +230,13 @@ function Home() {
                       clip-rule="evenodd"
                     ></path>
                   </svg>
-                  What is the difference between Windster and Tailwind UI?
+                  Will the app help me with meal prepping?
                 </h3>
                 <p class="text-gray-500 dark:text-gray-400">
-                  Although both Windster and Tailwind UI are built for
-                  integration with Tailwind CSS, the main difference is in the
-                  design, the pages, the extra components and UI elements that
-                  Windster includes.
-                </p>
-                <p class="text-gray-500 dark:text-gray-400">
-                  Additionally, Windster is a project that is still in
-                  development, and later it will include both the application,
-                  marketing, and e-commerce UI interfaces.
-                </p>
-              </div>
-              <div class="mb-10">
-                <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
-                  <svg
-                    class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                      clip-rule="evenodd"
-                    ></path>
-                  </svg>
-                  Can I use Windster in open-source projects?
-                </h3>
-                <p class="text-gray-500 dark:text-gray-400">
-                  Generally, it is accepted to use Windster in open-source
-                  projects, as long as it is not a UI library, a theme, a
-                  template, a page-builder that would be considered as an
-                  alternative to Windster itself.
-                </p>
-                <p class="text-gray-500 dark:text-gray-400">
-                  With that being said, feel free to use this design kit for
-                  your open-source projects.
-                </p>
-                <p class="text-gray-500 dark:text-gray-400">
-                  Find out more information by{" "}
-                  <a
-                    href="#"
-                    class="font-medium underline text-primary-600 dark:text-primary-500 hover:no-underline"
-                  >
-                    reading the license
-                  </a>
-                  .
+                  Yes! Plan your meals like a pro with our meal prepping
+                  feature. The app not only helps you organize your grocery list
+                  by sorting what you need to buy for your weekly meal prep but
+                  also tracks macros to keep your nutrition on point.
                 </p>
               </div>
             </div>
