0667ac03931a9474a4575ded7fa7248c0b65c950
Tyler Bray <tylerhbray@gmail.com>
2024-04-21 14:22:07 -0700

    changed search bar

--- view/src/Pages/Dashboard.js
+++ view/src/Pages/Dashboard.js
@@ -134,26 +134,6 @@ function Dashboard(props) {
   return (
     <div className="p-8 mx-auto max-w-7xl">
       <section>
-        <div className="container animate-fade">
-          {/* create a search bar menu with a search button */}
-          <div className="flex  items-center mb-8">
-            <h2 className="text-2xl font-bold mx-2">Search for recipes </h2>
-            <div>
-              <form className="flex space-x-4 mb-3" onSubmit={handleSubmit}>
-                <input
-                  type="text"
-                  className="px-4 py-2 border border-gray-300 rounded-md"
-                  placeholder="Search"
-                  onChange={(e) => setSearchTerm(e.target.value)}
-                />
-                <button className="px-4 py-2 bg-gray-200 rounded hover:bg-gray-400">
-                  Search
-                </button>
-              </form>
-            </div>
-          </div>
-          {/* create a filter menu with a filter button */}
-        </div>
         <h2 className="text-2xl font-bold mb-6 animate-fade">Categories</h2>
         <div className="flex space-x-4 mb-8 ">
           {/* Placeholder for category cards */}
@@ -177,6 +157,47 @@ function Dashboard(props) {
       <section>
         <h2 className="text-2xl font-bold mb-6 flex justify-between items-center animate-fade">
           <span>My Recipes</span>
+          <form class="ms-auto w-1/3" onSubmit={handleSubmit}>
+            <label
+              for="default-search"
+              class="mb-2 text-sm font-medium text-gray-900 sr-only white:text-dark"
+            >
+              Search
+            </label>
+            <div class="relative">
+              <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                <svg
+                  class="w-4 h-4 text-gray-500 dark:text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                  />
+                </svg>
+              </div>
+              <input
+                placeholder="Search"
+                onChange={(e) => setSearchTerm(e.target.value)}
+                type="Search"
+                id="default-search"
+                class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required
+              />
+              <button
+                type="submit"
+                class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Search
+              </button>
+            </div>
+          </form>
         </h2>
         <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-5 gap-4 mb-8">
           {/* Placeholder for recipe cards */}
